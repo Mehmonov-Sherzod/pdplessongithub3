44c17268b854893a7a3960e4615469ba24da7bf3
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-04-11 17:08:33 +0500

    class ochildi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿public class program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+    }
+}
